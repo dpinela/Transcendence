8903c621da6bf72183cf9286d0d595e07b9797b8
Diogo Pinela <diogoid7400@gmail.com>
2022-05-30 02:53:54 +0100

    Bump version number

--- Transcendence/Transcendence.cs
+++ Transcendence/Transcendence.cs
@@ -152,7 +152,7 @@ namespace Transcendence
             TextEdits.Add((key, sheetName), text);
         }
 
-        public override string GetVersion() => "1.1.2";
+        public override string GetVersion() => "1.2";
 
         private SaveSettings Settings = new();
 
