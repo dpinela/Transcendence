821ef71e6d67e2816276e20ac3935593b3eb558e
Diogo Pinela <diogoid7400@gmail.com>
2022-11-04 01:07:13 +0000

    Add structural versioning policy

--- Transcendence/RandoSettingsManagerProxy.cs
+++ Transcendence/RandoSettingsManagerProxy.cs
@@ -3,14 +3,14 @@ using RandoSettingsManager.SettingsManagement.Versioning;
 
 namespace Transcendence
 {
-    internal class RandoSettingsManagerProxy : RandoSettingsProxy<RandoSettings, int>
+    internal class RandoSettingsManagerProxy : RandoSettingsProxy<RandoSettings, Signature>
     {
         internal Func<RandoSettings> getter;
         internal Action<RandoSettings> setter;
 
         public override string ModKey => nameof(Transcendence);
 
-        public override VersioningPolicy<int> VersioningPolicy => new NoVersioningPolicy();
+        public override VersioningPolicy<Signature> VersioningPolicy => new StructuralVersioningPolicy() { settingsGetter = this.getter };
 
         public override bool TryProvideSettings(out RandoSettings? sent)
         {
---
+++ Transcendence/StructuralVersioningPolicy.cs
@@ -0,0 +1,66 @@
+using System.Text;
+using System.Security.Cryptography;
+using RandoSettingsManager.SettingsManagement.Versioning;
+
+namespace Transcendence
+{
+    internal class StructuralVersioningPolicy : VersioningPolicy<Signature>
+    {
+        internal Func<RandoSettings> settingsGetter;
+
+        public override Signature Version => new() { FeatureSet = FeatureSetForSettings(settingsGetter()) };
+
+        private static List<string> FeatureSetForSettings(RandoSettings rs) =>
+            SupportedFeatures.Where(f => f.feature(rs)).Select(f => f.name).ToList();
+
+        public override bool Allow(Signature s) => s.FeatureSet.All(name => SupportedFeatures.Any(sf => sf.name == name));
+
+        private static List<(Predicate<RandoSettings> feature, string name)> SupportedFeatures = new()
+        {
+            (rs => rs.AddCharms, "AddCharms"),
+            (rs => rs.IncreaseMaxCharmCostBy > 0, "IncreaseMaxCharmCost"),
+            (rs => rs.Logic.AntigravityAmulet, "AntigravLogic"),
+            (rs => rs.Logic.BluemothWings == GeoCharmLogicMode.OnWithGeo, "BluemothGeoLogic"),
+            (rs => rs.Logic.BluemothWings == GeoCharmLogicMode.On, "BluemothLogic"),
+            (rs => rs.Logic.SnailSoul, "SnailSoulLogic"),
+            (rs => rs.Logic.SnailSlash, "SnailSlashLogic"),
+            (rs => rs.Logic.MillibellesBlessing, "MillibelleLogic"),
+            (rs => rs.Logic.NitroCrystal, "NitroLogic"),
+            (rs => rs.Logic.Crystalmaster == GeoCharmLogicMode.OnWithGeo, "CrystalmasterGeoLogic"),
+            (rs => rs.Logic.Crystalmaster == GeoCharmLogicMode.On, "CrystalmasterLogic"),
+            (rs => rs.Logic.ChaosOrb != ChaosOrbMode.Off, "ChaosLogic"),
+            (rs => rs.Logic.AnyEnabled(), "LogicHash:" + LogicHash())
+        };
+
+        private static string LogicHash()
+        {
+            using var hash = SHA256.Create();
+            using var hstream = new CryptoStream(Stream.Null, hash, CryptoStreamMode.Write);
+            var modDir = Path.GetDirectoryName(typeof(StructuralVersioningPolicy).Assembly.Location);
+            foreach (var name in new string[] { "LogicMacros.json", "LogicPatches.json", "ConnectionLogicPatches.json" })
+            {
+                using (var logicFile = File.OpenRead(Path.Combine(modDir, name)))
+                {
+                    logicFile.CopyTo(hstream);
+                }
+            }
+            hash.TransformFinalBlock(new byte[] {}, 0, 0);
+            return ToHex(hash.Hash);
+        }
+
+        private static string ToHex(byte[] stuff)
+        {
+            var sb = new StringBuilder(stuff.Length * 2);
+            foreach (var b in stuff)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+            return sb.ToString();
+        }
+    }
+
+    internal struct Signature
+    {
+        public List<string> FeatureSet;
+    }
+}
