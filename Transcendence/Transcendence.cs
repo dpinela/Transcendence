685b12e63d772a7cf53e79ae5e6196e7f23d4cad
Diogo Pinela <diogoid7400@gmail.com>
2023-01-29 03:29:10 +0000

    Bump version number

--- Transcendence/Transcendence.cs
+++ Transcendence/Transcendence.cs
@@ -171,7 +171,7 @@ namespace Transcendence
         // breaks infinite loop when reading equippedCharm_X
         private bool Equipped(Charm c) => c.Settings(Settings).Equipped;
 
-        public override string GetVersion() => "1.3.6";
+        public override string GetVersion() => "1.4";
 
         internal SaveSettings Settings = new();
 
